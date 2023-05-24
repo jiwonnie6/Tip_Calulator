a82af296169194af2feeb14f8a27d8f9b2c5873e
Jiwon Hur <jiwonhur6@gmail.com>
2023-05-24 16:55:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-/*
-tip calculator
 
-must use three functions
-
-calculateBill()
-increasePeople()
-decreasePeople()
-*/
-
-// need ids for bill input, tip input, number of people, and per person total div
-
+// get DOM elements
 let billInput = document.getElementById('billTotalInput')
 let tipInput = document.getElementById('tipInput')
 let numberOfPeopleDiv = document.getElementById('numberOfPeople')
@@ -18,36 +8,46 @@ let perPersonTotalDiv = document.getElementById('perPersonTotal')
 let numberOfPeople = Number(numberOfPeopleDiv.innerText)
 
 const calculateBill = () => {
+    // get bill amount
     let bill = Number(billInput.value)
 
+    // get tip percentage
     let tipPercentage = Number(tipInput.value) / 100
 
+    // calculate tip amount
     let tipAmount = bill * tipPercentage
 
+    // calculate total bill amount
     let total = bill + tipAmount
 
+    // calculate the total per person
     let perPersonTotal = total / numberOfPeople
 
+    // update DOM element
     perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`
 
 }
 
 const increasePeople = () => {
+    // increment number of people
     numberOfPeople = numberOfPeople + 1
 
+    // update number 
     numberOfPeopleDiv.innerText = numberOfPeople
 
     calculateBill()
 }
 
 const decreasePeople = () => {
-
+    // if num of people is already 1 or less return
     if (numberOfPeople <= 1){
         return 
     }
     
+    // decrement number of people
     numberOfPeople = numberOfPeople - 1
 
+    // update number
     numberOfPeopleDiv.innerText = numberOfPeople
 
     calculateBill()
